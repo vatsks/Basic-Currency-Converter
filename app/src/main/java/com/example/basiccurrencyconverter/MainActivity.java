bd6673803b6adc1673a18220b4f498612d82fac4
vatsks <iamksvats@gmail.com>
2021-06-22 00:48:13 +0530

    Initial layout and indentation.

---
+++ app/src/main/java/com/example/basiccurrencyconverter/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.basiccurrencyconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    double value, fvalue;
+
+    public void click(View view){
+
+        TextView editTextAmt=findViewById(R.id.editTextAmt);
+         value = Double.parseDouble(editTextAmt.getText().toString());
+         fvalue = value*74.17;
+        String text = Double.toString(fvalue);
+        Toast.makeText(this,text,Toast.LENGTH_LONG).show();
+
+
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
